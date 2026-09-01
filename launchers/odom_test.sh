ac76a60da77e197f329eee0023cfdb08380a6ab1
Braden <bjz-github@spwiz.com>
2026-09-01 17:02:25 -0600

    Cleaned up odometry test and graphing

--- launchers/odom_test.sh
+++ launchers/odom_test.sh
@@ -8,7 +8,7 @@ dt-launchfile-init
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
-roslaunch odometry robot_odom.launch
+roslaunch odometry odometry.launch test:=true
 
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
